d62b9e76f796ebe8c6d2c622dc3a8e588f574eaa
Morpheus <nastisha.m@gmail.com>
2021-07-28 17:26:08 +0300

    hell

--- REX/ke.py
+++ REX/ke.py
@@ -1,9 +1,38 @@
 
 import subprocess
 import re
+
+def edit(text):
+    a=3
+    if text==3:
+        name = input("введите название репапапо")
+    a_file = open('/Users/macos/Documents/scripts/robot', "r")
+    list_of_lines = a_file.readlines()
+    a_file.close()
+    if text==3:
+        list_of_lines[1] = "cd /Users/macos/octopus/" + name + '\n'
+    if text==1:
+        a=int(input("Коммит по дефолту(0) или свой(1)?"))
+    if a==1:
+        s=input()
+        list_of_lines[3]="git commit -am "+s
+    a_file = open('/Users/macos/Documents/scripts/robot', "w")
+    a_file.writelines(list_of_lines)
+    a_file.close()
+
+
 def cycle(text):
     if (text == 1):
+        edit(text)
         subprocess.call("/Users/macos/Documents/scripts/robot")
+        default = "roboti zahvatyat mir { nechelovecheskii commit }"
+        a_file = open('/Users/macos/Documents/scripts/robot', "r")
+        list_of_lines = a_file.readlines()
+        a_file.close()
+        list_of_lines[3] = "git commit -am " + default
+        a_file = open('/Users/macos/Documents/scripts/robot', "w")
+        a_file.writelines(list_of_lines)
+        a_file.close()
     if (text == 2):
         f = open('/Users/macos/Documents/scripts/robot', 'r+')
         for line in f:
@@ -12,14 +41,7 @@ def cycle(text):
                 break
         f.close()
     if (text == 3):
-        name = input("введите название репапапо")
-        a_file = open('/Users/macos/Documents/scripts/robot', "r")
-        list_of_lines = a_file.readlines()
-        list_of_lines[1] = "cd /Users/macos/octopus/" + name + '\n'
-
-        a_file = open('/Users/macos/Documents/scripts/robot', "w")
-        a_file.writelines(list_of_lines)
-        a_file.close()
+        edit(text)
 
         # if (text == 4):
         #  img = cv2.imread('/Users/macos/Downloads/koza.png', 0)
